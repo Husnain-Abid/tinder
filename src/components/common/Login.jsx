56d223f574037f4c8a9d1118276a59c768c00e8e
Husnain-Abid <134037646+Husnain-Abid@users.noreply.github.com>
2025-03-25 17:06:52 +0500

    hj

--- src/components/common/Login.jsx
+++ src/components/common/Login.jsx
@@ -9,7 +9,7 @@ const LoginPopup = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 max-w-sm bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-xl w-full max-w-sm p-8 relative text-sm">
         {/* Close button */}
         <button
